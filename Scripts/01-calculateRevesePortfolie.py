d34fd78010dd2a442ab4e0e910d5cbfa408d3719
Yue <Cy1093662798@outlook.com>
2019-03-24 14:50:16 +0800

    更换计算反转组合收益、平均的组合收益的函数
    - 在01-calcu.. 文件中，更换计算每日反转收益组合的代码为封装的函数
    - 同上，更换计算平均反转组合收益的部分为封装过的函数

--- Scripts/01-calculateRevesePortfolie.py
+++ Scripts/01-calculateRevesePortfolie.py
@@ -34,41 +34,14 @@ ret_df_final.dropna(subset=['cum_ret'], inplace=True)
 # 计算每组按照dollar_volume 加权得到的组合收益率
 portfolie_ret_serie = rfunc.reverse_port_ret_mini(df=ret_df_final)
 
-
 # %% calculate every day, in every cap_group, the reverse portfolie
+# 低减高形成的反转收益率
+reverse_ret_each_day: pd.DataFrame = rfunc.reverse_port_ret_all(
+    serie=portfolie_ret_serie)
 
-
-# “输家-赢家”，计算反转收益的函数。
-def reverse_port(serie: pd.Series):
-    '''
-    输入不同标准收益率分组的一列序列，输家-赢家获得反转组合收益
-    serie: pd.Series 一列分组后的收益值，输家在前，赢家在后。
-    '''
-    result = []
-    for index in range(5):
-        high_group = serie.iloc[9 - index]
-        low_group = serie.iloc[index]
-        result.append(low_group - high_group)
-    return result
-
-
-# 按照日期和cap_group 分组后，每组内应用以上的函数求出反转收益。
-portfolie_ret_grouped = portfolie_ret_serie.groupby(['Trddt', 'cap_group'])
-reverse_ret_in_serie: pd.Series = portfolie_ret_grouped.apply(reverse_port)
-
-# 上面输出一个serie of list，把它转化为一个数据框。
-reverse_ret_each_day = pd.DataFrame(
-    (item for item in reverse_ret_in_serie),
-    index=reverse_ret_in_serie.index,
-    columns=['Lo-Hi', '2-9', '3-8', '4-7', '5-6'])
-reverse_ret_aver = reverse_ret_each_day.groupby('cap_group', sort=False).mean()
-
-# 计算均值后cap_group 失去了Categories 类型，重新规定为CategoricalIndex 并排序
-reverse_ret_aver.set_index(
-    pd.CategoricalIndex(
-        reverse_ret_aver.index, categories=['Small', '2', '3', '4', 'Big']),
-    inplace=True)
-reverse_ret_aver.sort_index()
+# 各个日期的反转收益求均值
+reverse_ret_aver: pd.DataFrame = reverse_ret_each_day.groupby(
+    'cap_group', sort=False).mean()
 
 # %%
 # store = pd.HDFStore('data/reverse_portfolie.h5')
