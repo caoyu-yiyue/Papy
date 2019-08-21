4e9dc43f196302e67a9c65eee01bdef62b276d16
Yue <Cy1093662798@outlook.com>
2019-08-21 14:23:09 +0800

    增加了快速生成一个报告文本的脚本
    将该文本在jupyter 环境中运行，传入一个process data 存储的路径，将根据这组数据生成一个快速报告结果

---
+++ src/models/view_result.py
@@ -0,0 +1,82 @@
+"""
+将该脚本在jupyter 环境中运行，传入processed 数据存储的位置，可以快速生成一个报告
+"""
+# %%
+from src.models.grouped_ols import GroupedOLS
+from src.models.ols_model import OLSFeatures
+from IPython.display import display, HTML
+import warnings
+import sys
+
+warnings.filterwarnings('ignore')
+
+# %%
+# 生成所有需要的对象
+if __name__ == "__main__":
+    proc_data_in = sys.argv[1]
+else:
+    proc_data_in = 'data/processed/'
+
+all_GroupedOLS_obj = dict.fromkeys([e.name for e in OLSFeatures])
+
+for fea_type in list(OLSFeatures):
+    obj = GroupedOLS(processed_dir=proc_data_in, ols_features=fea_type)
+    all_GroupedOLS_obj[fea_type.name] = obj
+
+
+# %%
+# 直接查看结果的函数
+def t_test_single_col(for_which, col):
+    """
+    查看某一列的系数与t 值
+    """
+    for key in for_which:
+        display(HTML('<h3>{}</h3>'.format(key)))
+        obj: GroupedOLS = all_GroupedOLS_obj[key]
+        if isinstance(col, str):
+            msg_col = col
+        elif isinstance(col, int):
+            fea_name = obj.look_up_ols_detail(detail='params_name')
+            msg_col = fea_name[col]
+        display(HTML("<p>param for '{}'</p>".format(msg_col)))
+        display(obj.look_up_ols_detail(detail='param', column=col))
+
+        display(HTML("<p>t value for '{}'</p>".format(msg_col)))
+        display(obj.look_up_ols_detail(detail='t_test_star', column=col))
+
+
+# 快速查看多列的系数和为0 的检验t 值
+def t_test_multi_col(for_which, cols):
+    for key in for_which:
+        display(HTML('<h3>{}</h3>'.format(key)))
+        delta_std_obj: GroupedOLS = all_GroupedOLS_obj[key]
+        fea_name: list = delta_std_obj.look_up_ols_detail(detail='params_name')
+        cols_slc = slice(cols[0], cols[1])
+        test_str = ' + '.join(fea_name[cols_slc]) + ' = 0'
+        display(HTML("<p>test for '{}'.".format(test_str)))
+        display(
+            delta_std_obj.look_up_ols_detail(detail='t_test_star',
+                                             t_test_str=test_str))
+
+
+# %% [markdown]
+# ## rm and rolling_std_log
+t_test_single_col(for_which=['market_ret'], col='const')
+
+# %% [markdown]
+# ## rolling_std_log
+t_test_single_col(for_which=['rolling_std_log'], col='rolling_std_log')
+
+# ## delta_std and with rm
+t_test_multi_col(for_which=['delta_std', 'delta_std_and_rm'], cols=(1, 6))
+
+# %% [markdown]
+# ## delta_std_full and with rm
+t_test_single_col(for_which=['delta_std_full', 'delta_std_full_rm'], col=1)
+
+# %% [markdown]
+# ## std_with_sign, delta_std_full_sign, delta_std_full_sign_rm
+t_test_multi_col(for_which=[
+    'std_with_sign', 'delta_std_full_sign', 'delta_std_full_sign_rm'
+],
+                 cols=(2, 4))
